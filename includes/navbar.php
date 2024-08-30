18c1dc75ec94ea014c12edcc620476ec45de95af
duodils <epimpim8@gmail.com>
2024-08-30 09:39:45 +0700

    Menambahkan halaman home sampai about

---
+++ includes/navbar.php
@@ -0,0 +1,12 @@
+<nav class="nav">
+    <div class="menu-icon"></div>
+    <img src="assets/images/logo.png" alt="" class="logo">
+    <ul class="ul-item">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#ticket">Ticket</a></li>
+        <li><a href="#layout">Layout</a></li>
+        <li><a href="#gallery">Gallery</a></li>
+    </ul>
+    <img src="assets/images/user.png" alt="" class="user">
+</nav>
